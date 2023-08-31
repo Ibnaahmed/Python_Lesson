2d48e926c0c5a0b061dc048d92f43083d6443dba
Ibnaahmed <ibnaahmednur@ggmail.com>
2023-08-31 09:41:57 +0600

    Python_practice

---
+++ Python_practice.py
@@ -0,0 +1,12 @@
+# Exercise _number 2
+time=int(input("Enter the timespane:  "))
+if(time<=10):
+    print("Good Morning, Sir.")
+
+elif(time<=16):
+    print("Good Noon,Sir")
+
+elif(time<=19):
+    print("Goood Afternoon")
+else:
+    print("Good night")
